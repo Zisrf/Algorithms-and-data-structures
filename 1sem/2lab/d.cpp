1c3a31ba2d6251fa37ee5d743a0429431cf40cbc
Ivan Zelenin <zeleninivans@gmail.com>
2022-02-10 20:34:17 +0300

    Update 2nd lab

---
+++ 1sem/2lab/d.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream cin("antiqs.in");
+    ofstream cout("antiqs.out");
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        a[i] = i + 1;
+
+    for (int i = 1; i < n; ++i)
+        swap(a[i], a[i / 2]);
+
+    for (int i = 0; i < n; ++i)
+        cout << a[i] << ' ';
+    cout << endl;
+
+    cin.close();
+    cout.close();
+    return 0;
+}
